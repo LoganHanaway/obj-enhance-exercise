598c1c1d630b86ab6ede67df9546d8de807c2c3a
Logan Hanaway <lthanaway14@gmail.com>
2024-02-21 18:15:27 -0600

    finished

--- app.js
+++ app.js
@@ -35,3 +35,50 @@ const mathStuff = {
         return a * a;
     }
 }
+
+// computed property names
+
+// 'periwinkle' '9c88ff'
+// 'bright ube' ' D6A3E8'
+
+const color = {
+    periwinkle: '9c88ff',
+    '9c88ff': 'periwinkle'
+};
+
+// function makeColor(name, hex){
+//     return {
+//     name : hex,
+//     hex: name
+// };
+// }
+
+
+// function makeColor(name, hex){
+//     const color = {};
+//     color[name] = hex;
+//     color[hex] = name;
+//     return color;
+// }
+
+function makeColor(name, hex){
+    return {
+        [name] : hex,
+        [hex] : name
+    };
+}
+
+
+// shorthand version of bellow
+const mystery = {
+    [6 +7]: 'thirteen'
+}
+
+// const obj = {};
+// obj[6+7] = 'thirteen';
+
+// this is essentially what mystery is doing
+function _defineProperty(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
